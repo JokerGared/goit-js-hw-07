d9380a128b652b49ec35327a577e78d48264c4cd
Oleh <therkasoa@gmail.com>
2025-02-03 23:00:12 +0200

    another fix made

--- js/task-4.js
+++ js/task-4.js
@@ -3,11 +3,11 @@ const form = document.querySelector('.login-form');
 form.addEventListener('submit', onSubmitPress);
 function onSubmitPress(evt) {
   evt.preventDefault();
-  const email = evt.target.elements.email.value;
-  const password = evt.target.elements.password.value;
+  const email = evt.target.elements.email.value.trim();
+  const password = evt.target.elements.password.value.trim();
   const emailKey = evt.target.elements.email.name;
   const passwordKey = evt.target.elements.password.name;
-  if (email === '' || password.trim() === '') {
+  if (email === '' || password === '') {
     alert('All form fields must be filled in');
     return;
   }
